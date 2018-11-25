2013338743c9f560170fe7e0e1eeb5411a86b5e3
Ken Stark <ken@craftedup.com>
2018-11-25 12:19:11 -0500

    front page icons

--- front-page.php
+++ front-page.php
@@ -16,11 +16,44 @@ $dir = get_stylesheet_directory_uri();
     <div class="col-sm-4">
       <h2>The Link and Connections to your Customers</h2>
     </div>
-    <div class="vidOver_btm">
-      <h2>Experience the Difference</h2>
+  </div>
+  <div class="row">
+    <div class="col-sm-10 col-sm-push-1 col-md-8 col-md-push-2">
+      <?php the_content(); ?>
+    </div>
+  </div>
+  <div class="vidOver_btm">
+    <h2>Experience the Difference</h2>
+  </div>
+</div>
+
+<?php if(have_rows('icon_links')): ?>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-12">
+      <div class="linkIcons">
+        <?php while( have_rows('icon_links')) : the_row(); 
+        $awesomeIcon = get_sub_field('font_awesome_icon');
+        $imgIcon = get_sub_field('image_icon');
+        $title = get_sub_field('title');
+        $link = get_sub_field('page_link'); ?>
+        <div class="iconBox">
+          <a href="<?php echo $link; ?>" title="<?php echo $title; ?>">
+            <?php
+            if($awesomeIcon) : 
+              echo $awesomeIcon;
+            elseif($imgIcon):
+              echo '<img src="'.$imgIcon.'" alt="'.$title.'">';
+            endif; ?>
+            <h4><?php echo $title; ?></h4>
+          </a>
+        </div>
+        <?php endwhile; ?>
+      </div>
     </div>
   </div>
 </div>
+<?php endif; ?>
 
 
 
