0efb698a231e23414ea9fd4fecc7255d02c9d75a
Ken Stark <ken@craftedup.com>
2020-11-15 13:51:37 -0500

    top header changes

--- header.php
+++ header.php
@@ -29,7 +29,7 @@ if(is_front_page()) {
 						<button>Track Order</button>
 					</form>
 					<div class="topContact">
-						<p>317-602-8892<a href="/contact-us/">Contact Us</a></p>
+						<p>317-851-8798<a href="/contact-us/">Connect With Us</a></p>
 					</div>
 				</div>
 			</div>
